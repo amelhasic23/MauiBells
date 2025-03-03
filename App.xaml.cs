cd88488e739361949f545236e4401368e493c3d3
Amel Hasić <amelhasic23@outlook.com>
2025-03-03 09:40:53 +0100

    Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiBells;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage());
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiBells;
+
+using Calendar;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder.UseMauiApp<App>()
+			   .ConfigureMauiHandlers(handlers =>
+			   {
+#if ANDROID
+				   handlers.AddHandler<CalendarView, CalendarMaterialHandler>();
+#else
+				   handlers.AddHandler<CalendarView, CalendarHandler>();
+#endif
+			   });
+
+		return builder.Build();
+	}
+}
